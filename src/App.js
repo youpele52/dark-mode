ccb488412d0c25c2df7672085f5308cb105c9177
Youpele Michael <mjayjesus@gmail.com>
2021-03-11 23:20:30 +0100

    Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
 import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
+import Switch from './Switch'
+
+// save and check the users prefered theme, so that anytime we reload the page remains at the user prefered theme
+const getStorageTheme = () => {
+  let theme = 'dark-theme'
+  let toggleValue = 'false'
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme')
+    toggleValue = localStorage.getItem('toggleValue')
+  }
+  return { theme, toggleValue }
+}
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getStorageTheme().theme)
+  const [value, setValue] = useState(getStorageTheme().toggleValue)
+  const toggleTheme = () => {
+    if (theme === 'dark-theme') {
+      setValue(!value)
+      setTheme('light-theme')
+    } else {
+      setTheme('dark-theme')
+      setValue(!value)
+    }
+  }
+
+  useEffect(() => {
+    // here we are accessing the HTML document
+    document.documentElement.className = theme
+
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  return (
+    <main>
+      <nav>
+        <div className='nav-center'>
+          <h1>Youpele's Poems</h1>
+          {/* <button className='btn' onClick={toggleTheme}>
+            toggle
+          </button> */}
+
+          <Switch isOn={value} onColor='#4fa6f7' handleToggle={toggleTheme} />
+
+          {/* <div
+            onClick={() => {
+              toggleTheme()
+            }}
+          >
+            <label htmlFor='toggle' className='switch'>
+              <input
+                type='checkbox'
+                id='toggle'
+                name='toggle'
+                checked={checked}
+                onChange={(e) => {
+                  console.log(e.target.value)
+                }}
+              />
+              <span className='slider round' onClick={toggleTheme}></span>
+            </label>
+          </div> */}
+        </div>
+      </nav>
+      <section className='articles'>
+        {data.map((poem) => {
+          return (
+            <Article key={poem.id} {...poem}>
+              {' '}
+            </Article>
+          )
+        })}
+      </section>
+    </main>
+  )
 }
 
 export default App
--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+const Article = ({ title, date, length, snippet, link }) => {
+  return (
+    <article className='post'>
+      <a href={link}>
+        <h2>{title}</h2>
+      </a>
+      <div className='post-info'>
+        {/* using moment.js here */}
+        <span> {moment(date).format('dddd Do, MMMM ,  YYYY.')}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  )
 }
 
 export default Article
